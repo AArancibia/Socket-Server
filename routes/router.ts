2e7232c10b2db555001a12e1dbbb43f4d5b6db1e
Alexis Joel Arancibia Sanchez <alexis2396@hotmail.com>
2019-02-01 22:32:26 -0500

    Primer Commit - Rest Server

---
+++ routes/router.ts
@@ -0,0 +1,41 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get( '/mensaje', ( req: Request, res: Response , next )  => {
+    res.json({
+        ok: true,
+        mensaje: 'Todo esta bien',
+    });
+});
+
+router.post( '/mensaje', ( req: Request, res: Response , next )  => {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+
+    res.json({
+        ok: true,
+        cuerpo,
+        de,
+    });
+
+});
+
+router.post( '/mensaje/:id', ( req: Request, res: Response , next )  => {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+
+    const id = req.params.id;
+
+    res.json({
+        ok: true,
+        cuerpo,
+        de,
+        id,
+    });
+
+});
+
+export default router;
